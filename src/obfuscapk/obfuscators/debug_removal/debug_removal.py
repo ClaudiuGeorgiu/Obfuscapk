45c945e9f152fef73e24ebeb48a0200812e2e647
Gabriel Claudiu Georgiu <gabriel@claudiugeorgiu.com>
2019-08-25 20:42:23 +0200

    Fix debug info removal

--- src/obfuscapk/obfuscators/debug_removal/debug_removal.py
+++ src/obfuscapk/obfuscators/debug_removal/debug_removal.py
@@ -23,10 +23,7 @@ class DebugRemoval(obfuscator_category.ICodeObfuscator):
                               '.local ', '.end local', '.restart local',
                               '.param ']
 
-            param_with_annotation_pattern = re.compile(r'\.param.+?'
-                                                       r'\.annotation.+?'
-                                                       r'\.end annotation\n'
-                                                       r'\s+\.end param\n', re.UNICODE | re.MULTILINE | re.DOTALL)
+            param_pattern = re.compile(r'\s+\.param\s(?P<register>[vp0-9]+)')
 
             for smali_file in util.show_list_progress(obfuscation_info.get_smali_files(),
                                                       interactive=obfuscation_info.interactive,
@@ -37,15 +34,26 @@ class DebugRemoval(obfuscator_category.ICodeObfuscator):
                     file_content = current_file.read()
 
                 with open(smali_file, 'w', encoding='utf-8') as current_file:
-                    # Remove .param <annotation> .end param.
-                    file_content = param_with_annotation_pattern.sub('', file_content)
-
                     # Keep only the lines not containing debug op codes.
-                    lines_to_keep = []
-                    for line in file_content.splitlines(keepends=True):
-                        if not any(line.strip().startswith(op_code) for op_code in debug_op_codes):
-                            lines_to_keep.append(line)
-                    current_file.writelines(lines_to_keep)
+                    # ".param <annotation> .end param" shouldn't be removed.
+                    reversed_lines_to_keep = []
+                    inside_param_declaration = False
+                    for line in reversed(file_content.splitlines(keepends=True)):
+                        if line.strip().startswith('.end param'):
+                            inside_param_declaration = True
+                            reversed_lines_to_keep.append(line)
+                        elif line.strip().startswith('.param ') and inside_param_declaration:
+                            inside_param_declaration = False
+                            # Remove unnecessary data from param (name and type comment).
+                            line = '{0}\n'.format(param_pattern.match(line).group())
+                            reversed_lines_to_keep.append(line)
+                        elif not inside_param_declaration:
+                            if not any(line.strip().startswith(op_code) for op_code in debug_op_codes):
+                                reversed_lines_to_keep.append(line)
+                        else:
+                            reversed_lines_to_keep.append(line)
+
+                    current_file.writelines(list(reversed(reversed_lines_to_keep)))
 
         except Exception as e:
             self.logger.error('Error during execution of "{0}" obfuscator: {1}'.format(self.__class__.__name__, e))
