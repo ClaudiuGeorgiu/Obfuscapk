19fb7f9afc0370a3b5430d338f120674ff0d1dd4
Gabriel Claudiu Georgiu <gabriel@claudiugeorgiu.com>
2020-04-28 14:04:13 +0200

    Add tests for command line interface

--- src/obfuscapk/cli.py
+++ src/obfuscapk/cli.py
@@ -1,11 +1,10 @@
-#!/usr/bin/env python3.7
-# coding: utf-8
+#!/usr/bin/env python3
 
 import argparse
 import logging
 
-from .main import perform_obfuscation, check_external_tool_dependencies
-from .obfuscator_manager import ObfuscatorManager
+from obfuscapk.main import perform_obfuscation, check_external_tool_dependencies
+from obfuscapk.obfuscator_manager import ObfuscatorManager
 
 logger = logging.getLogger(__name__)
 
@@ -21,61 +20,114 @@ def get_cmd_args(args: list = None):
     obfuscators = ObfuscatorManager().get_obfuscators_names()
 
     parser = argparse.ArgumentParser(
-        prog='python3.7 -m obfuscapk.cli',
-        description='Obfuscate an application (.apk) without needing its source code.')
-    parser.add_argument('apk_file', type=str, metavar='<APK_FILE>',
-                        help='The path to the application (.apk) to obfuscate')
-    parser.add_argument('-o', '--obfuscator', action='append', metavar='OBFUSCATOR', choices=obfuscators,
-                        help='The name of the obfuscator to use. Can be specified multiple times to use more '
-                             'obfuscators (in sequence). Allowed values are: {0}'.format(', '.join(obfuscators)),
-                        required=True)
-    parser.add_argument('-w', '--working-dir', type=str, metavar='DIR',
-                        help='The working directory that will contain the intermediate files. By default a directory '
-                             'will be created in the same directory as the input application. If the specified '
-                             'directory doesn\'t exist, it will be created')
-    parser.add_argument('-d', '--destination', type=str, metavar='OUT_APK',
-                        help='The path where to save the obfuscated .apk file. By default the file will be saved '
-                             'in the working directory')
-    parser.add_argument('-i', '--ignore-libs', action='store_true',
-                        help='Ignore known third party libraries during the obfuscation operations')
-    parser.add_argument('-p', '--show-progress', action='store_true', dest='interactive',
-                        help='Show obfuscation progress (as a progress bar)')
-    parser.add_argument('-k', '--virus-total-key', action='append', metavar='VT_API_KEY',
-                        help='When using Virus Total obfuscator, a valid API key has to be provided. '
-                             'Can be specified multiple times to use a different API key for each request '
-                             '(cycling through the API keys)')
+        prog="python3 -m obfuscapk.cli",
+        description="Obfuscate an application (.apk) without needing its source code.",
+    )
+    parser.add_argument(
+        "apk_file",
+        type=str,
+        metavar="<APK_FILE>",
+        help="The path to the application (.apk) to obfuscate",
+    )
+    parser.add_argument(
+        "-o",
+        "--obfuscator",
+        action="append",
+        metavar="OBFUSCATOR",
+        choices=obfuscators,
+        help="The name of the obfuscator to use. Can be specified multiple times to "
+        "use more obfuscators (in sequence). Allowed values are: {0}".format(
+            ", ".join(obfuscators)
+        ),
+        required=True,
+    )
+    parser.add_argument(
+        "-w",
+        "--working-dir",
+        type=str,
+        metavar="DIR",
+        help="The working directory that will contain the intermediate files. By "
+        "default a directory will be created in the same directory as the input "
+        "application. If the specified directory doesn't exist, it will be created",
+    )
+    parser.add_argument(
+        "-d",
+        "--destination",
+        type=str,
+        metavar="OUT_APK",
+        help="The path where to save the obfuscated .apk file. By default the file "
+        "will be saved in the working directory",
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore-libs",
+        action="store_true",
+        help="Ignore known third party libraries during the obfuscation operations",
+    )
+    parser.add_argument(
+        "-p",
+        "--show-progress",
+        action="store_true",
+        dest="interactive",
+        help="Show obfuscation progress (as a progress bar)",
+    )
+    parser.add_argument(
+        "-k",
+        "--virus-total-key",
+        action="append",
+        metavar="VT_API_KEY",
+        help="When using Virus Total obfuscator, a valid API key has to be provided. "
+        "Can be specified multiple times to use a different API key for each request "
+        "(cycling through the API keys)",
+    )
     return parser.parse_args(args)
 
 
-if __name__ == '__main__':
+def main():
     """
     A full command to obfuscate an application:
 
-    python3.7 -m obfuscapk.cli -p -i -w /working/dir/path -d /path/to/obfuscated.apk -o DebugRemoval \
-    -o CallIndirection -o MethodRename -o LibEncryption -o AssetEncryption -o MethodOverload \
-    -o ConstStringEncryption -o ResStringEncryption -o ArithmeticBranch -o FieldRename -o Nop -o Goto \
-    -o ClassRename -o Reflection -o AdvancedReflection -o Reorder -o RandomManifest -o Rebuild \
-    -o NewSignature -o NewAlignment -o VirusTotal -k virus_total_key_1 -k virus_total_key_2 \
+    python3 -m obfuscapk.cli -p -i -w /working/dir/path -d /path/to/obfuscated.apk \
+    -o DebugRemoval -o CallIndirection -o MethodRename \
+    -o LibEncryption -o AssetEncryption -o MethodOverload -o ConstStringEncryption \
+    -o ResStringEncryption -o ArithmeticBranch -o FieldRename -o Nop -o Goto \
+    -o ClassRename -o Reflection -o AdvancedReflection -o Reorder -o RandomManifest \
+    -o Rebuild -o NewSignature -o NewAlignment \
+    -o VirusTotal -k virus_total_key_1 -k virus_total_key_2 \
     /path/to/original.apk
     """
 
-    # Verify that the external dependencies are available even before showing the help message: this way, if the help
-    # message is displayed correctly it means that all the needed external tools are available and ready to be used.
+    # Verify that the external dependencies are available even before showing the help
+    # message: this way, if the help message is displayed correctly it means that all
+    # the needed external tools are available and ready to be used.
     check_external_tool_dependencies()
 
     arguments = get_cmd_args()
 
     if arguments.apk_file:
-        arguments.apk_file = arguments.apk_file.strip(' \'"')
+        arguments.apk_file = arguments.apk_file.strip(" '\"")
 
     if arguments.working_dir:
-        arguments.working_dir = arguments.working_dir.strip(' \'"')
+        arguments.working_dir = arguments.working_dir.strip(" '\"")
 
     if arguments.destination:
-        arguments.destination = arguments.destination.strip(' \'"')
+        arguments.destination = arguments.destination.strip(" '\"")
 
     if arguments.virus_total_key:
-        arguments.virus_total_key = [key.strip(' \'"') for key in arguments.virus_total_key]
+        arguments.virus_total_key = [
+            key.strip(" '\"") for key in arguments.virus_total_key
+        ]
 
-    perform_obfuscation(arguments.apk_file, arguments.obfuscator, arguments.working_dir, arguments.destination,
-                        arguments.interactive, arguments.ignore_libs, arguments.virus_total_key)
+    perform_obfuscation(
+        arguments.apk_file,
+        arguments.obfuscator,
+        arguments.working_dir,
+        arguments.destination,
+        arguments.interactive,
+        arguments.ignore_libs,
+        arguments.virus_total_key,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test/test_cli.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import os
+
+import pytest
+
+from obfuscapk import cli
+
+# noinspection PyUnresolvedReferences
+from test.test_fixtures import (
+    tmp_working_directory_path,
+    tmp_demo_apk_v10_original_path,
+)
+
+
+class TestCommandLine(object):
+    def test_valid_basic_command_without_quotes(
+        self,
+        tmp_working_directory_path: str,
+        tmp_demo_apk_v10_original_path: str,
+        monkeypatch,
+    ):
+        obfuscated_apk_path = os.path.join(tmp_working_directory_path, "obfuscated.apk")
+
+        # Mock the command line parser.
+        arguments = cli.get_cmd_args(
+            "-w {working_dir} -d {destination} "
+            "-o Rebuild -o NewSignature -o NewAlignment {apk_file}".format(
+                working_dir=tmp_working_directory_path,
+                destination=obfuscated_apk_path,
+                apk_file=tmp_demo_apk_v10_original_path,
+            ).split()
+        )
+        monkeypatch.setattr(cli, "get_cmd_args", lambda: arguments)
+
+        cli.main()
+
+        assert os.path.isfile(obfuscated_apk_path)
+
+    def test_valid_basic_command_with_quotes(
+        self,
+        tmp_working_directory_path: str,
+        tmp_demo_apk_v10_original_path: str,
+        monkeypatch,
+    ):
+        obfuscated_apk_path = os.path.join(tmp_working_directory_path, "obfuscated.apk")
+
+        # Mock the command line parser.
+        arguments = cli.get_cmd_args(
+            "-w '{working_dir}' -d \"{destination}\" "
+            "-o Rebuild -k \"key1\" -k 'key2' '{apk_file}'".format(
+                working_dir=tmp_working_directory_path,
+                destination=obfuscated_apk_path,
+                apk_file=tmp_demo_apk_v10_original_path,
+            ).split()
+        )
+        monkeypatch.setattr(cli, "get_cmd_args", lambda: arguments)
+
+        cli.main()
+
+        assert os.path.isfile(obfuscated_apk_path)
+
+    def test_missing_required_parameters(self, monkeypatch):
+        # Mock the command line parser.
+        original = cli.get_cmd_args
+        monkeypatch.setattr(cli, "get_cmd_args", lambda: original([]))
+
+        with pytest.raises(SystemExit) as e:
+            cli.main()
+        assert e.value.code == 2
+
+    def test_missing_external_tool(self, monkeypatch):
+        monkeypatch.setenv("APKTOOL_PATH", "invalid.apktool.path")
+
+        # Mock the command line parser.
+        arguments = cli.get_cmd_args("-o Rebuild ignore.apk".split())
+        monkeypatch.setattr(cli, "get_cmd_args", lambda: arguments)
+
+        with pytest.raises(RuntimeError) as e:
+            cli.main()
+        assert "Something is wrong with executable" in str(e.value)
